60735333bff76e596fe757a018f362d7de3a58ea
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-22 10:44:08 -0400

    Clean up and refactor into functions

--- gtfs.py
+++ gtfs.py
@@ -3,17 +3,18 @@ from dotenv import load_dotenv
 import os
 import requests
 
-load_dotenv()
 
+def get_feed(feed, url):
+    response = requests.get(url, allow_redirects=True)
+    feed.ParseFromString(response.content)
+    with open('output.txt', mode='w') as f:
+        for entity in feed.entity:
+            if entity.HasField('trip_update'):
+                f.write(str(entity.trip_update))
+
+load_dotenv()
 feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get(
-    ('http://datamine.mta.info/mta_esi.php?key='
+url = ('http://datamine.mta.info/mta_esi.php?key='
         + os.getenv("API_KEY")
-    + '&feed_id=1'),
-    allow_redirects=True
-)
-feed.ParseFromString(response.content)
-with open('output.txt', mode='w') as f:
-    for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            f.write(str(entity.trip_update))
+        + '&feed_id=1')
+get_feed(feed, url)
